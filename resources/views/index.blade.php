ce15a22980232a7ec65c2cee392a4ada74fe8b4a
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-03-22 03:29:11 +0000

    1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+{{-- メインコンテンツ --}}
+@section('contents')
+<h1>ログイン</h1>
+    @if ($errors->any())
+        <div>
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+        </div>
+    @endif
+    <form action="login" method="post">
+        @csrf
+        email:<input name="email"><br>
+        パスワード<input name="password" type="password"><br>
+        <button>ログインする</button><br>
+        <a href="/user/register">会員登録</a>
+    </form>
+@endsection
